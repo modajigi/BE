2551628c3dd82d9de037e302749008ea240efdcf
hyunzinbak <hgggny@gmail.com>
2023-11-19 15:56:52 +0900

    test2

--- src/main/java/com/sideproject/modazin/dto/UserSignUpReq.java
+++ src/main/java/com/sideproject/modazin/dto/UserSignUpReq.java
@@ -4,4 +4,11 @@ package com.sideproject.modazin.dto;
 public class UserSignUpReq {
     private String email;
     private String nickName;
+    private String password;
+    private int longtitude;
+    private int latitude;
+    private String phoneNumber;
+    private String profile;
+
+
 }
