d9a526fb214eed5483cda3b8275e181d0a8c4e00
hyunzinbak <hgggny@gmail.com>
2023-11-28 22:50:28 +0900

    jpa 초기 test 세팅

--- src/main/java/com/sideproject/modazin/ModazinApplication.java
+++ src/main/java/com/sideproject/modazin/ModazinApplication.java
@@ -1,12 +1,10 @@
 package com.sideproject.modazin;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) // DB 연결 비활성화
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) // DB 연결 비활성화
 public class ModazinApplication {
 
     public static void main(String[] args) {
