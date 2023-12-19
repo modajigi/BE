008543881d1287d710d810fc331df3e378b41053
soli <eumsoli@gmail.com>
2023-12-19 17:44:10 +0900

    soli/issue/#8 alter user

--- src/main/java/com/sideproject/modazin/controller/user/UserController.java
+++ src/main/java/com/sideproject/modazin/controller/user/UserController.java
@@ -1,33 +1,7 @@
 package com.sideproject.modazin.controller.user;
 
-import com.sideproject.modazin.dto.user.ResponseDto;
-import com.sideproject.modazin.dto.user.UserDto;
-import com.sideproject.modazin.service.user.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user")
-@RequiredArgsConstructor
 public class UserController {
-
-    private final UserService userService;
-
-    @GetMapping("/signup")
-    public String signup() {
-        return "users/signup";
-    }
-
-//    @PostMapping("/signup")
-//    public ResponseEntity signup(@RequestBody UserDto userDto) {
-//        return userService.signup(userDto);
-//    }
-
-
 }
--- src/main/java/com/sideproject/modazin/repository/user/UserRepository.java
+++ src/main/java/com/sideproject/modazin/repository/user/UserRepository.java
@@ -1,9 +1,9 @@
 package com.sideproject.modazin.repository.user;
 
-import com.sideproject.modazin.dto.user.UserDto;
+import com.sideproject.modazin.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserDto, Long> { //JpaRepository<Entity 클래스, PK 타입>
+public interface UserRepository extends JpaRepository<User, Integer> { // dto로 변경하기
 
 
 }
